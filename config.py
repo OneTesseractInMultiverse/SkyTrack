a8e1fece13cd929e08deb0c07a42afc8e738be6c
Pedro Guzmán <pedro@subvertic.com>
2017-11-28 20:02:13 -0600

    Redis added to persist the chain. Some configurations added

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 SECRET_KEY = 'Ac2Sed1Se2F34Sw12'
+REDIS_URL = "redis://localhost:6379/0"
--- skychain/__init__.py
+++ skychain/__init__.py
@@ -4,6 +4,7 @@
 import os
 import uuid
 from flask import Flask
+from flask_redis import FlaskRedis
 from skychain.models.blockchain import Blockchain
 
 # ------------------------------------------------------------------------------
@@ -13,6 +14,9 @@ __version__ = '1.0.0'
 sky_app = Flask('Brick')
 sky_app.config.from_object('config')
 sky_app.debug = True
+
+# Connect to redis
+redis_store = FlaskRedis(sky_app)
 
 # Generate a globally unique address for this node
 node_id = str(uuid.uuid4()).replace('-', '')
